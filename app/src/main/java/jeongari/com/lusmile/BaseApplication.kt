392561675f5b89730811dadf68e4d43f2c33bc71
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-03-23 21:07:22 +0900

    Leakcanary is inherited to BaseApplication

---
+++ app/src/main/java/jeongari/com/lusmile/BaseApplication.kt
@@ -0,0 +1,14 @@
+package jeongari.com.lusmile
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if(LeakCanary.isInAnalyzerProcess(this)){
+            return
+        }
+        LeakCanary.install(this)
+    }
+}
