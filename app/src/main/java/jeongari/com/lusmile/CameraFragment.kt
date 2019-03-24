5a4a2d1e95297632eec3d914d99f503d7ca74761
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-03-24 09:49:25 +0900

    ByteArray for producing FirebaseVisionImage added

--- app/src/main/java/jeongari/com/lusmile/CameraFragment.kt
+++ app/src/main/java/jeongari/com/lusmile/CameraFragment.kt
@@ -7,17 +7,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import jeongari.com.camera.Camera2BasicFragment
 
 
 class CameraFragment : Camera2BasicFragment() {
+
+    private var byteArray: ByteArray? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View? {
         val view = inflateFragment(R.id.layoutFrame, inflater, container)
         return view
     }
 
-    override fun detectFace() {}
+    override fun detectFace() {
+        val bitmap = textureView?.getBitmap(textureView!!.width, textureView!!.height)
+        if (bitmap != null) {
+
+        }
+    }
 
     companion object {
 
