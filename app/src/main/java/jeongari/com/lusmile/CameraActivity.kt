a2b76f6337fc8571af2f94d350837582c79bd48c
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-03-24 09:08:12 +0900

    Rename the single activity into 'CameraActivity'

--- app/src/main/java/jeongari/com/lusmile/CameraActivity.kt
+++ app/src/main/java/jeongari/com/lusmile/CameraActivity.kt
@@ -3,10 +3,10 @@ package jeongari.com.lusmile
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_camera)
     }
 }
